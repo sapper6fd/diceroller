a16a2aa9d88eaca4d3d7b8a761a7f25fadbe201a
Justin <dums39@hotmail.com>
2018-05-28 14:07:20 -0600

    Added comments to better describe the method actions

--- DiceMethods3/DiceMethods3/Program.cs
+++ DiceMethods3/DiceMethods3/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,26 +15,31 @@ namespace DiceMethods3
 
         static void startGame()
         {
+            // Declare start game question
             string choice = string.Empty;
             
             do
             {
+                // Display intro message to user
                 Lines();
                 Console.WriteLine("Welcome to the Dice Roll game!");
                 Console.WriteLine("Do you want to toll the dics?");
                 Lines();
+                // Get users decision to start the game
                 choice = Console.ReadLine().ToLower();
                 if (choice == "yes")
                 {
+                    // Display the result of the dice being rolled.
                     int die = 0;
                     die = rollGenerator();
                     Console.WriteLine($"You rolled a {die}");
                 }
-
+                // End the game at the users request
                 else if (choice == "no")
                 {
                     Console.WriteLine("GAME OVER");
                 }
+                // Inform user of an invalid option to start or end the game
                 else if (choice != "yes" || choice != "no")
                 {
                     Console.WriteLine("Please enter a Yes or No answer");
@@ -45,9 +50,11 @@ namespace DiceMethods3
 
         static void Lines()
         {
+            // Add seperating lines to make the game easier to read
             Console.WriteLine("-------------------------------");
         }
 
+        // Generate a random number from 1, up to and including 6, each time the dice is rolled.
         static int rollGenerator()
         {
             int die = 0;
